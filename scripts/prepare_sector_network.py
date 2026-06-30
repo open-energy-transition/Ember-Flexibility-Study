d31a2473050228d45455e2d044e0c2a6f613d861
martacki <martha.frysztacki@gmail.com>
2026-06-30 13:56:22 +0200

    bugfix

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -1654,7 +1654,7 @@ def insert_electricity_distribution_grid(
             suffix=" rooftop",
             bus=n.generators.loc[solar, "bus"] + " low voltage",
             carrier="solar rooftop",
-            p_nom_extendable=True if "solar rooftop" in extendable_carriers else False,
+            p_nom_extendable=True if "solar rooftop" in extendable_carriers.get("Generator", []) else False,
             p_nom_max=potential.loc[solar],
             marginal_cost=n.generators.loc[solar, "marginal_cost"],
             capital_cost=costs.at["solar-rooftop", "capital_cost"],
